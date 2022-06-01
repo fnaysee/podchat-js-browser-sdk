acae47f44251118179c7b28119ab5e5ecbcf2784
wp-src <wpmagic70@gmail.com>
2022-06-01 13:32:53 +0430

    version

--- examples/bundle-test.js
+++ examples/bundle-test.js
@@ -58918,12 +58918,12 @@ WildEmitter.mixin(WildEmitter);
                         if(screenShareInfo.isStarted() && screenShareInfo.getOwner() === messageContent[0].userId)
                             callStateController.removeScreenShareFromCall()
 
-                        if(messageContent[0].userId === chatMessaging.userInfo.id) {
+/*                        if(messageContent[0].userId === chatMessaging.userInfo.id) {
                             chatEvents.fireEvent('callEvents', {
                                 type: 'CALL_ENDED',
                                 callId: threadId
                             });
-                        }
+                        }*/
                     }
 
                     //If I'm the only call participant, stop the call
