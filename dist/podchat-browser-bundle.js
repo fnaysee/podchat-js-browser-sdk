91054cd80926e1bdd6b4672fdecb2d3891a3f839
Farhan <farhan.naysee@gmail.com>
2023-08-16 15:02:44 +0330

    Version

--- dist/podchat-browser-bundle.js
+++ dist/podchat-browser-bundle.js
@@ -44136,7 +44136,7 @@ WildEmitter.mixin = function (constructor) {
 WildEmitter.mixin(WildEmitter);
 
 },{}],267:[function(require,module,exports){
-module.exports={"version":"12.9.7-snapshot.13","date":"۱۴۰۲/۵/۲۵","VersionInfo":"Release: false, Snapshot: true, Is For Test: true"}
+module.exports={"version":"12.9.7-snapshot.14","date":"۱۴۰۲/۵/۲۵","VersionInfo":"Release: false, Snapshot: true, Is For Test: true"}
 },{}],268:[function(require,module,exports){
 "use strict";
 
