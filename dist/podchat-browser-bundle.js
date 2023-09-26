ee3e8937fa5cb87cc7a01525e88615e52b7cc602
Farhan <farhan.naysee@gmail.com>
2023-09-26 13:48:09 +0330

    Version

--- dist/podchat-browser-bundle.js
+++ dist/podchat-browser-bundle.js
@@ -45916,7 +45916,7 @@ FilterXSS.prototype.process = function (html) {
 module.exports = FilterXSS;
 
 },{"./default":275,"./parser":277,"./util":278,"cssfilter":124}],280:[function(require,module,exports){
-module.exports={"version":"12.9.7-snapshot.29","date":"۱۴۰۲/۷/۴","VersionInfo":"Release: false, Snapshot: true, Is For Test: true"}
+module.exports={"version":"12.9.7-snapshot.30","date":"۱۴۰۲/۷/۴","VersionInfo":"Release: false, Snapshot: true, Is For Test: true"}
 },{}],281:[function(require,module,exports){
 "use strict";
 
