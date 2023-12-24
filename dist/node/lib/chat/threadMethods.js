32443d7598a3d80c2c4c1f6435e5e0d5947d2bd3
Farhan <farhan.naysee@gmail.com>
2023-12-24 11:38:08 +0330

    Build

--- dist/node/lib/chat/threadMethods.js
+++ dist/node/lib/chat/threadMethods.js
@@ -12,13 +12,15 @@ var _constants = require("../constants");
 var _utility = _interopRequireDefault(require("../../utility/utility"));
 
 function ThreadMethods(app) {
+  var pinMessageRequests = {};
+
   function getPinMessages(params, callback) {
     var sendData = {
       chatMessageVOType: _constants.chatMessageVOTypes.GET_PIN_MESSAGE,
       typeCode: app.sdkParams.generalTypeCode,
       //params.typeCode,
-      token: app.sdkParams.token,
-      content: params.content
+      token: app.sdkParams.token // content: params.content
+
     };
 
     if (!params.uniqueId) {
@@ -29,27 +31,72 @@ function ThreadMethods(app) {
       return;
     }
 
+    var mustRequestIds = [],
+        existingItems = [];
     params.content.forEach(function (item) {
       var thread = app.store.threads.findOrCreate({
         id: item
       });
+
+      if (!thread.isPinMessageRequested() && !thread.pinMessageVO) {
+        mustRequestIds.push(item);
+      } else if (thread.pinMessageVO) {
+        existingItems.push(JSON.parse(JSON.stringify({
+          id: thread.id,
+          pinMessageVO: thread.pinMessageVO
+        })));
+      }
     });
-    app.store.threads.getPinMessages(params.content);
-    return app.messenger.sendMessage(sendData); //     onResult: function (result) {
+
+    if (existingItems.length) {
+      app.chatEvents.fireEvent('threadEvents', {
+        type: 'GET_PIN_MESSAGES',
+        result: existingItems,
+        uniqueId: sendData.uniqueId
+      });
+    }
+
+    if (mustRequestIds.length) {
+      pinMessageRequests[sendData.uniqueId] = mustRequestIds;
+      return app.messenger.sendMessage(sendData);
+    } //     onResult: function (result) {
     //
     //         callback && callback(result);
     //     }
     // });
+
   }
 
   function onGetPinMessages(uniqueId, messageContent, contentCount) {
-    app.chatEvents.fireEvent('threadEvents', {
-      type: 'GET_PIN_MESSAGES',
-      result: messageContent,
-      uniqueId: uniqueId
-    }); // if (app.store.messagesCallbacks[uniqueId]) {
+    if (pinMessageRequests[uniqueId]) {
+      var result = [];
+      pinMessageRequests[uniqueId].forEach(function (it) {
+        var th = app.store.threads.findOrCreate({
+          id: it.id
+        });
+        var serverResult = messageContent.length && messageContent.find(function (item) {
+          return item.id == th.id;
+        });
+
+        if (serverResult) {
+          th.pinMessage.setPinMessage(serverResult.pinMessageVO);
+          result.push(JSON.parse(JSON.stringify({
+            id: th.id,
+            pinMessageVO: serverResult.pinMessageVO
+          })));
+        }
+
+        th.pinMessage.setPinMessageRequested(true);
+      });
+      app.chatEvents.fireEvent('threadEvents', {
+        type: 'GET_PIN_MESSAGES',
+        result: result,
+        uniqueId: uniqueId
+      });
+    } // if (app.store.messagesCallbacks[uniqueId]) {
     //     app.store.messagesCallbacks[uniqueId](Utility.createReturnData(false, '', 0, messageContent, contentCount, uniqueId));
     // }
+
   }
 
   return {
